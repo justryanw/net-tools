95449f3ead5ef2ff72750e05b8bea5141825971e
Ryan Walker <ryanjwalker2001@gmail.com>
2024-06-26 11:18:08 +0100

    speed

--- flake.nix
+++ flake.nix
@@ -2,19 +2,27 @@
   inputs = {
     utils.url = "github:numtide/flake-utils";
   };
-  outputs = { self, nixpkgs, utils }: utils.lib.eachDefaultSystem (system:
+  outputs = { nixpkgs, utils, ... }: utils.lib.eachDefaultSystem (system:
     let
       pkgs = nixpkgs.legacyPackages.${system};
-    in
-    {
+
+    in rec {
+      packages = {
+	speed = pkgs.writeShellScriptBin "speed" ''
+	  ssh $1 "iperf -s -1 -D" && iperf -c $1 && pkill iperf
+	'';
+      };
+
       devShell = pkgs.mkShell {
-        buildInputs = with pkgs; [
+        buildInputs = (with pkgs; [
 	  wirelesstools
 	  owl
 	  iw
 	  nmap
 	  busybox
-        ];
+        ]) ++ [
+	  packages.speed
+	];
       };
     }
   );
